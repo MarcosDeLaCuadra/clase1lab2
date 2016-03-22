36405d1c7e547d03e4753e9e0b1293cd21138e6a
MarcosDeLaCuadra <maros_kb@hotmail.com>
2016-03-21 23:40:14 -0300

    ejercicio 3 numeros primos falta poneren mensaje el numero 2 y 3

---
+++ asdasdspractica/asdasds/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace asdasds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int numero;
+            int aux;
+            Console.WriteLine("ingrese numero");
+            while (int.TryParse(Console.ReadLine(), out numero) == false)
+            {
+                Console.WriteLine("reingrese numero");
+            }
+
+            aux = numero;
+
+            for (int i = numero; i > 0; i--)
+            {
+                
+                if(i%2!=0 && i%3!=0)
+                {
+                 Console.WriteLine(i);
+                }
+              
+            }
+         
+          
+      /*     aux = numero % 2;
+            
+           
+
+            aux = numero % 3;
+
+           // cadena = Console.ReadLine();
+            Console.WriteLine(aux);*/
+
+        }
+    }
+}
---
+++ ejercicio3numerosprimos/ejercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+           // string aux;
+            string aux;
+
+            Console.WriteLine("ingrese un numero");
+            while (int.TryParse(aux, out numero) == false)
+            {
+                Console.WriteLine("error reintente ingresando un numero");
+                aux = Console.ReadLine();
+
+                Console.WriteLine("{0}", numero);
+            }
+        }
+    }
+}
